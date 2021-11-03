e3026a9b4c38f6b241d56ee97c6146cba6ed5cb7
ebrahim <e.mottaghi.rezaei@gmail.com>
2021-11-04 00:32:12 +0330

    Adding ALSBaseAIController class.

---
+++ Plugins/CALSv4/Source/CALSv4/Core/AI/ALSBaseAIController.cpp
@@ -0,0 +1,1 @@
+#include "ALSBaseAIController.h"
---
+++ Plugins/CALSv4/Source/CALSv4/Core/AI/ALSBaseAIController.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ALSBaseAIController.generated.h"
+
+UCLASS()
+class CALSV4_API AALSBaseAIController : public AAIController {
+	GENERATED_BODY()
+
+};
