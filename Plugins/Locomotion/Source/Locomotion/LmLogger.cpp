66b4b29e099ab7283dd4d8a28ddb6ed7a97aec15
ebrahim <e.mottaghi.rezaei@gmail.com>
2022-07-31 21:18:49 +0430

    Adding the Option to print only to logs

--- Plugins/Locomotion/Source/Locomotion/LmLogger.cpp
+++ Plugins/Locomotion/Source/Locomotion/LmLogger.cpp
@@ -1,21 +1,21 @@
 #include "LMLogger.h"
 
-void ULmLogger::LogInfo(const FString Message, const float duration) {
-	if (GEngine)
+void ULmLogger::LogInfo(const FString Message, const float duration, const bool PrintOnScreen) {
+	if (GEngine && PrintOnScreen)
 		GEngine->AddOnScreenDebugMessage(-1, duration, FColor::Cyan, FString::Printf(TEXT("Locomotion plugin| %s"), *Message));
 
 	UE_LOG(LogTemp, Display, TEXT("Locomotion plugin| %s"), *Message);
 }
 
-void ULmLogger::LogWarning(const FString Message, const float duration) {
-	if (GEngine)
+void ULmLogger::LogWarning(const FString Message, const float duration, const bool PrintOnScreen) {
+	if (GEngine && PrintOnScreen)
 		GEngine->AddOnScreenDebugMessage(-1, duration, FColor::Orange, FString::Printf(TEXT("Locomotion plugin| %s"), *Message));
 
 	UE_LOG(LogTemp, Warning, TEXT("Locomotion plugin| %s"), *Message);
 }
 
-void ULmLogger::LogError(const FString Message, const float duration) {
-	if (GEngine)
+void ULmLogger::LogError(const FString Message, const float duration, const bool PrintOnScreen) {
+	if (GEngine && PrintOnScreen)
 		GEngine->AddOnScreenDebugMessage(-1, duration, FColor::Red, FString::Printf(TEXT("Locomotion plugin| %s"), *Message));
 
 	UE_LOG(LogTemp, Error, TEXT("Locomotion plugin| %s"), *Message);
--- Plugins/Locomotion/Source/Locomotion/LmLogger.h
+++ Plugins/Locomotion/Source/Locomotion/LmLogger.h
@@ -11,9 +11,9 @@ class LOCOMOTION_API ULmLogger : public UBlueprintFunctionLibrary {
 	GENERATED_BODY()
 public:
 	UFUNCTION(BlueprintCallable, Category = "Locomotion|Logging")
-		static void LogInfo(FString Message, const float duration = 5.0f);
+		static void LogInfo(FString Message, const float duration = 5.0f, const bool PrintOnScreen = true);
 	UFUNCTION(BlueprintCallable, Category = "Locomotion|Logging")
-		static void LogWarning(FString Message, const float duration = 5.0f);
+		static void LogWarning(FString Message, const float duration = 5.0f, const bool PrintOnScreen = true);
 	UFUNCTION(BlueprintCallable, Category = "Locomotion|Logging")
-		static void LogError(FString Message, const float duration = 5.0f);
+		static void LogError(FString Message, const float duration = 5.0f, const bool PrintOnScreen = true);
 };
